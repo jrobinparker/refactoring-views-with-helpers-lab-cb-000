21e27338e7fe78634279195ec6579f1c43412a1e
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 02:57:08 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
 
  def display_artist(song)
-    if @song.artist.present?
+    if song.artist.present?
       link_to @artist.name, artist_path(@artist)
     else 
       link_to "Add Artist", edit_song_path(song)
